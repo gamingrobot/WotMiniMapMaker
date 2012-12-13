758a181386d2faeb5d5e598ac97421197ce2539c
Juha Kuitunen <kuitsi@iki.fi>
2012-12-13 19:39:32 +0200

    added new script for linux to rename compressed jpg maps

--- readme.txt
+++ readme.txt
@@ -1,3 +1,19 @@
+First time:
+- remove FolderHolder.txt files
+- extract icons from res/packages/gui.pkg/gui/flash/Minimap.swf with SWFExtract into icon/
+  - you need at least base flags and starting points, IDs ~280-350, rename those to:
+    - green.png
+    - gs1.png
+    - gs2.png
+    - gs3.png
+    - red.png
+    - rs1.png
+    - rs2.png
+    - rs3.png
+    - blank.png
+- set WoT installation path in makeMiniMaps.sh (Linux only)
+
+Usage (Windows):
 unpack all packages and put the maps into mapsRaw
 remove all .chunk .cdata files via del /S *.chunk from the mapsRaw directory
 run imgconverter.bat to convert all dds to png
@@ -8,6 +24,10 @@ run makemaps.py
 copy all maps to gridding in
 run gridding.py
 move all out to mapsFinal
+
+Usage (Linux):
+ - execute makeMiniMaps.sh
+ - optionally execute renameJpg.sh
 
 Requirements:
 - Python 2.x
@@ -24,19 +44,4 @@ Linux:
   - imgconverter.sh
   - copyRawMaps.sh
   - compressMaps.sh
-
-First run:
-- remove FolderHolder.txt files
-- extract icons from res/packages/gui.pkg/gui/flash/Minimap.swf with SWFExtract into icon/
-  - you need at least base flags and starting points, IDs ~280-350, rename those to:
-    - green.png
-    - gs1.png
-    - gs2.png
-    - gs3.png
-    - red.png
-    - rs1.png
-    - rs2.png
-    - rs3.png
-    - blank.png
-- set WoT installation path in makeMiniMaps.sh
-- execute makeMiniMaps.sh
+  - renameJpg.sh
---
+++ renameJpg.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+for file in ./jpg/*
+do
+	BN=`basename "${file}" .jpg`
+
+	# destroy all but map id and game mode from file name
+	IDMODE=`echo $BN | sed -r 's/_.*_/_/'`
+
+	ID=(`echo $IDMODE | cut -d "_" -f 1`)
+	MODE=(`echo $IDMODE | cut -d "_" -f 2`)
+
+	case "$ID" in
+	"01") NAME="karelia"; ;;
+	"02") NAME="malinovka"; ;;
+	"03") NAME="province"; ;;
+	"04") NAME="himmelsdorf"; ;;
+	"05") NAME="prokhorovka"; ;;
+	"06") NAME="ensk"; ;;
+	"07") NAME="lakeville"; ;;
+	"08") NAME="ruinberg"; ;;
+	"10") NAME="mines"; ;;
+	"11") NAME="murovanka"; ;;
+	"13") NAME="erlenberg"; ;;
+	"14") NAME="siegfried_line"; ;;
+	"15") NAME="komarin"; ;;
+	"17") NAME="widepark"; ;;
+	"18") NAME="cliff"; ;;
+	"19") NAME="abbey"; ;;
+	"22") NAME="swamp"; ;;
+	"23") NAME="westfield"; ;;
+	"28") NAME="sand_river"; ;;
+	"29") NAME="el_halluf"; ;;
+	"31") NAME="airfield"; ;;
+	"33") NAME="fjords"; ;;
+	"34") NAME="redshire"; ;;
+	"35") NAME="steppes"; ;;
+	"36") NAME="fishermans_bay"; ;;
+	"37") NAME="mountain_pass"; ;;
+	"38") NAME="arctic_region"; ;;
+	"39") NAME="south_coast"; ;;
+	"42") NAME="port"; ;;
+	"44") NAME="live_oaks"; ;;
+	"45") NAME="highway"; ;;
+	"47") NAME="serene_coast"; ;;
+	"51") NAME="dragon_ridge"; ;;
+	*)
+		echo "WARNING: Unknown map ID" $ID
+		continue;
+	;;
+	esac
+
+	case "$MODE" in
+	"ctf") MODE=""; ;;
+	"dom") MODE="_encounter"; ;;
+	"ass") MODE="_assault"; ;;
+	esac
+
+	echo "renaming maps..."
+	mv -v "$file" "./jpg/$NAME$MODE.jpg"
+
+done
